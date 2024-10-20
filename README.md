4d4df9331f67ccea6bef81d3322b3bb45cd97a07
Syyren <129496054+Syyren@users.noreply.github.com>
2024-10-19 21:48:02 -0600

    updated controller to include mongo con. readme includes pips.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CPRO2201-Midterm_Project
- 
+
+pip i:
+python-dotenv
+"pymongo[srv]"
--- utils/Controllers/TaskController.py
+++ utils/Controllers/TaskController.py
@@ -1,1 +1,18 @@
-#For handling interraction with the view
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+#For handling interraction with mongo and the view
+
+#getting the connection path from the .env file and connecting to the client
+load_dotenv()
+uri = os.getenv("MONGO_CONNECTION")
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+#sending a ping to confirm successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
