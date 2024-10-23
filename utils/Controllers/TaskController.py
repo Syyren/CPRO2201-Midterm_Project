f55a4fc98f3f44bdd9b933be542852321a9f1c13
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-10-22 23:08:54 -0600

    getAllTasks() functioning as intended, other methods awaiting proper testing and functionality improvments

--- utils/Controllers/TaskController.py
+++ utils/Controllers/TaskController.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from ..Models.Task import Task
-# import datetime
-
 #For handling interraction with mongo and the view
 
 #getting the connection path from the .env file and connecting to the client
@@ -14,25 +12,17 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["Tasks"]
 collection = db[f"Regular_Tasks"]
 
-#sending a ping to confirm successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
 
-    # try:
-    #     #just a small injection
-    #     task = Task('testTask1','testing the create task method')
-    #     Task.createTask(task)
-    # except Exception as e:
-    #     print("secondary error: \n",e)
-
-except Exception as e:
-    print(e)
 
 #retreiving all tasks in collection as cursor instance
-def getTask():
+def getAllTasks():
     cursor = collection.find({})
     return cursor
+
+
+#INCOMPLETE
+def getTask(id):
+    return collection.find_one({ "_id": id })
 
 def createTask(task : Task):
     db_task = {
@@ -44,14 +34,10 @@ def createTask(task : Task):
     task_id = collection.insert_one(db_task).inserted_id
     print(f"Insert Successful! Given ID: {task_id}")
 
-def updateTask(task: Task):
-    db_task = {
-        "title" : f"{task.getTitle()}",
-        "description" : f"{task.getDescription()}",
-        "due_date" : f"{task.getDueDate()}",
-        "creation_date" : f"{task.getCreationDate()}"
-    }
-    collection.update_one(db_task)
+
+#INCOMPLETE
+def updateTask(task: Task, updated_key_value: dict):
+    collection.update_one({"_id": task.getId()}, {"$set": updated_key_value})
     print(f"Update Successful For ID:")
 
 def deleteTask(task_id):
@@ -59,3 +45,14 @@ def deleteTask(task_id):
 
 
 
+#sending a ping to confirm successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    # try:
+    #     print("getting a task...")
+    #     print(getTask("67187ff4475432be422d0dc3"))
+    # except Exception as e:
+    #     print('error getting task')
+except Exception as e:
+    print(e)
