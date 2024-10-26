5b7eb47cc73d536a14058a0416ff5eb78a1c582b
Syyren <129496054+Syyren@users.noreply.github.com>
2024-10-26 17:28:05 -0600

    edit view implemented, debugging still needed for dynamic elements

--- utils/Scripts/QOL.py
+++ utils/Scripts/QOL.py
@@ -12,13 +12,49 @@ def applyCSS():
 
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-def editTask(task : Task):
-    with st.expander(f"Editing task: {task}", expanded=True):
-        st.write(f"Make your changes to task: {task}")
+def editTask(task,
+             task_type : str,
+             date_check_text = "Is there a due date?",
+             title_txt = "Title*",
+             desc_txt = "Description",
+             due_txt = "Due Date",
+             submit_txt = "Submit",
+             PER = "Personal",
+             WOR = "Work"):
+    date_check = st.checkbox(date_check_text)
+    if task_type == PER:
+        friend_slider = st.slider("Friends", 0, 5, len(task.getFriends()))
+    elif task_type == WOR:
+        collaborator_slider = st.slider("Collaborators", 0, 8, len(task.getCollaborators()))
+    with st.form(f"edit_form_{hash(task.getId())}"):
+        title = st.text_input(title_txt, value=task.getTitle())
+        description = st.text_input(desc_txt, value=task.getDescription())
+        due_date = task.getDueDate()
+        if date_check:
+            due_date = st.date_input(due_txt, value=task.getDueDate())
+        if task_type == PER:
+            friends = []
+            for i in range(friend_slider):
+                friend_name = st.text_input(f"Friend {i + 1}'s Name", task.getFriends()[i])
+                if friend_name:
+                    friends.append(friend_name)
+        elif task_type == WOR:
+            collaborators = []
+            for i in range(collaborator_slider):
+                collaborator_name = st.text_input(f"Collaborator {i + 1}'s Name", task.getCollaborators()[i])
+                if collaborator_name:
+                    collaborators.append(collaborator_name)
+        submitted = st.form_submit_button(submit_txt)
+        if submitted:
+            st.write(f"\"{title}\" update submitted.")
+
+def cancelButton(task : Task):
+    cancel = st.button(f"Cancel", key=f"cancel_{hash(task.getId())}")
+    if cancel:
+        return False
 
 def deleteTask(task: Task):
-    with st.expander(f"Deleting task: \"{task}\"", expanded=True):
-        st.write(f"Are you sure you would like to delete \"{task}\"?")
+    st.write(f"Are you sure you would like to delete \"{task.getTitle()}\"?")
 
 #returns a string that's formatted depending on the type of task
 def taskString(task : Task, extra_data : str = "<p></p>"):
@@ -32,39 +68,40 @@ def taskString(task : Task, extra_data : str = "<p></p>"):
         {extra_data}
         {due_date}
         <p class="created">Created on: {task.getCreationDate().strftime("%a, %b %d, %Y at %I:%M%p")}</p>
-    </div>  
+    </div>
     '''
     return string
 
-def buttonLayout(task : Task):
-    col1, col2 = st.columns([1, 1])  
-    with col1:
-        if st.button(f"Edit \"{task.getTitle()}\"", key=f"edit_{hash(task)}"):
-            editTask(task)
-    with col2:
-        if st.button(f"Delete \"{task.getTitle()}\"", key=f"delete_{hash(task)}"):
-            deleteTask(task)
+def taskLayout(task_type : str, type_list : list):
+    for task in type_list:
+        col1, col2 = st.columns([1,1])
+        with col1:
+            edit = st.button(f"Edit {task.getTitle()}", key=f"checkbox_{hash(task.getId())}")
+        if edit:
+            with col2:
+                edit = cancelButton(task)
+            editTask(task, task_type)
+        else:
+            taskDisplay(task, task_type)
+            print(f"{task_type} task pulled: {task.getTitle()}")
 
-#function that writes details for a regular task
-def printRegTask(task : Task):
-    st.markdown(taskString(task), unsafe_allow_html=True)
-    buttonLayout(task)
-
-#function that writes details for a personal task
-def printPersonalTask(task : PersonalTask):
-    friends = ""
-    if len(task.getFriends()) > 0:
+#function that writes details for a task
+def taskDisplay(task : Task, task_type : str):
+    REG = "Regular"
+    PER = "Personal"
+    WOR = "Work"
+    if task_type == REG:
+        st.markdown(taskString(task), unsafe_allow_html=True)
+    elif task_type == PER:
+        friends = ""
+        if len(task.getFriends()) > 0:
             friends = f"<p>With: {printList(task.getFriends())}</p>"
-    st.markdown(taskString(task, friends), unsafe_allow_html=True)
-    buttonLayout(task)
-
-#function that writes details for a work task
-def printWorkTask(task : WorkTask):
-    collaborators = ""
-    if len(task.getCollaborators()) > 0:
-            collaborators = f"<p>Collaborators: {printList(task.getCollaborators())}</p>"
-    st.markdown(taskString(task, collaborators), unsafe_allow_html=True)
-    buttonLayout(task)
+        st.markdown(taskString(task, friends), unsafe_allow_html=True)
+    elif task_type == WOR:
+        collaborators = ""
+        if len(task.getCollaborators()) > 0:
+                collaborators = f"<p>Collaborators: {printList(task.getCollaborators())}</p>"
+        st.markdown(taskString(task, collaborators), unsafe_allow_html=True)
 
 #function that checks a list and then prints the tasks accordingly
 def taskPrint(type_list : list):
@@ -72,17 +109,11 @@ def taskPrint(type_list : list):
         st.error("No tasks available")
         print("Selected empty list")
     elif type(type_list[0]) is Task:
-        for task in type_list:
-            printRegTask(task)
-            print(f"Regular task pulled: {task.getTitle()}")
+        taskLayout("Regular", type_list)
     elif type(type_list[0]) is PersonalTask:
-        for task in type_list:
-            printPersonalTask(task)
-            print(f"Personal task pulled: {task.getTitle()}")
+        taskLayout("Personal", type_list)
     elif type(type_list[0]) is WorkTask:
-        for task in type_list:
-            printWorkTask(task)
-            print(f"Work task pulled: {task.getTitle()}")
+        taskLayout("Work", type_list)
 
 #function that prints a list in a nice readable way
 def printList(list : list):
