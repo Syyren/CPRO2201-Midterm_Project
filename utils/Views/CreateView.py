b28e0d940f063e023431fa57d2c0bea1ee5d066b
Syyren <129496054+Syyren@users.noreply.github.com>
2024-10-26 14:01:21 -0600

    futher enhancements to create view

--- utils/Views/CreateView.py
+++ utils/Views/CreateView.py
@@ -17,8 +17,9 @@ def taskSubmitted(title : str, description : str, due_date : datetime):
     st.write(f"Due Date: {due_date}")
 
 def formGenerator(form_name, 
-                  date_check_text = "Would you like to submit a due date?",
-                  title_txt = "Title", desc_txt = "Description",
+                  date_check_text = "Is there a due date?",
+                  title_txt = "Title*",
+                  desc_txt = "Description",
                   due_txt = "Due Date",
                   submit_txt = "Submit",
                   PER = "Personal",
@@ -28,7 +29,7 @@ def formGenerator(form_name,
         friend_slider = st.slider("Friends", 0, 5, 0)
     elif form_name == WOR:
         collaborator_slider = st.slider("Collaborators", 0, 8, 0)
-    with st.form(form_name):
+    with st.form(f"{form_name}_Form"):
         title = st.text_input(title_txt),
         description = st.text_input(desc_txt)
         due_date = None
@@ -38,15 +39,23 @@ def formGenerator(form_name,
             friends = []
             for i in range(friend_slider):
                 friend_name = st.text_input(f"Friend {i + 1}'s Name")
-                friends.append(friend_name)
+                if friend_name:
+                    friends.append(friend_name)
         elif form_name == WOR:
             collaborators = []
             for i in range(collaborator_slider):
                 collaborator_name = st.text_input(f"Collaborator {i + 1}'s Name")
-                collaborators.append(collaborator_name)
+                if collaborator_name:
+                    collaborators.append(collaborator_name)
         submitted = st.form_submit_button(submit_txt)
         if submitted:
             taskSubmitted(title, description, due_date)
+            if form_name == PER:
+                if len(friends) > 0:
+                    st.write(f"Friends: {printList(friends)}")
+            elif form_name == WOR:
+                if len(collaborators) > 0:
+                    st.write(f"Collaborators: {printList(collaborators)}")
 
 #function that defines the view when create is selected, allows user to make new task
 def createView(REG : str = "Regular", PER : str = "Personal", WOR : str = "Work"):
