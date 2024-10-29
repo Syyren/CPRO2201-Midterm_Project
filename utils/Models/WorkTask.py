d2acb56a2801e07616bea00a60def676ef639378
Syyren <129496054+Syyren@users.noreply.github.com>
2024-10-29 14:04:09 -0600

    adjusted commenting and last minute data consistency

--- utils/Models/WorkTask.py
+++ utils/Models/WorkTask.py
@@ -15,23 +15,30 @@ collaborators: {self.getCollaborators()}''')
 
     def getLength(self):
         return self._length
-
+    
+    #sets the length via two inputted numbers rather than the standard string
     def setLengthWithValues(self, length_hrs: int, length_mins: int):
         if length_mins in range(0, 61) and length_hrs in range(0, 13):
             self._length = f"{length_hrs}:{length_mins}"
         else:
             self._length = "0:0"
 
+    #sets the length via a string with some in-built error handling
     def setLength(self, length: str):
-        self._length = length
-
-    def getLengthMins(self):
-        mins = int(self.getLength().split(":")[1])
-        return mins
+        if len(length.split(":")) == 2 and length.split(":")[0].isdigit() and length.split(":")[1].isdigit() :
+            self._length = length
+        else:
+            self._length = "0:0"
     
+    #get the length's hours
     def getLengthHrs(self):
         hrs = int(self.getLength().split(":")[0])
         return hrs
+    
+    #gets the length's minutes
+    def getLengthMins(self):
+        mins = int(self.getLength().split(":")[1])
+        return mins
 
     def getCollaborators(self):
         return self._collaborators
@@ -42,7 +49,8 @@ collaborators: {self.getCollaborators()}''')
 #just testing
 if __name__ == "__main__":
     test_work_task = WorkTask("Meeting")
-    test_work_task.setLengthWithValues(2, 30)
+    #test_work_task.setLengthWithValues(2, 30)
+    test_work_task.setLength("test")
     test_work_task.displayAttributes()
     print("Hrs: ",test_work_task.getLengthHrs())
     print("Mins: ",test_work_task.getLengthMins())
